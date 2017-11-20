3b31c047c242e34f63f70e7396aff72d041b1a3a
marktubu <admin@AppledeMacBook-Pro.local>
2017-11-21 00:54:33 +0800

    add source file

---
+++ src/renderEngine/RawModel.java
@@ -0,0 +1,23 @@
+package renderEngine;
+
+public class RawModel {
+	
+	private int vaoID;
+	private int vertexCount;
+	
+	public RawModel(int vaoID, int vertexCount)
+	{
+		this.vaoID = vaoID;
+		this.vertexCount = vertexCount;
+	}
+	
+	public int getVaoID()
+	{
+		return this.vaoID;
+	}
+	
+	public int getVertexCount()
+	{
+		return this.vertexCount;
+	}
+}
